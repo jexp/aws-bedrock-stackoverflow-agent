f46b0c62641e8ae7939889d7a045156d797b5b4f
Rafal Janicki <rafal@kult.io>
2023-12-01 13:07:22 +0100

    Unified quotes

--- handler.py
+++ handler.py
@@ -1,6 +1,4 @@
 import json
-import os
-import pandas as pd
 
 from neo4j import GraphDatabase
 from aws_lambda_powertools.logging.logger import Logger
@@ -10,20 +8,19 @@ logger = Logger()
 
 @logger.inject_lambda_context
 def so_questions(event, context):
-    inputText = event.get('inputText','no input text')
-    params = ",".join([str(p['name']) + ":" + str(p['value']) for p in event.get('parameters',[])])
+    input_text = event.get('inputText', 'no input text')
+    params = ",".join([str(p['name']) + ":" + str(p['value']) for p in event.get('parameters', [])])
 
-    bodyParams = event.get('requestBody',{}).get('content',{}).get('application/json',{}).get('properties',[])
-    bodyParams = ",".join([str(p['name']) + ":" + str(p['value']) for p in bodyParams])
+    body = event.get('requestBody', {}).get('content', {}).get('application/json', {}).get('properties', [])
+    body = ",".join([str(p['name']) + ":" + str(p['value']) for p in body])
 
-    tag = "neo4j" # event['pathParameters']['tag'] or "neo4j" 
-    search = "expand" # event['queryParameters']['search'] or "expand"
-    database = "stackoverflow2"
+    tag = 'neo4j'  # event['pathParameters']['tag'] or 'neo4j'
+    search = 'expand'  # event['queryParameters']['search'] or 'expand'
+    database = 'stackoverflow2'
     limit = 3
-    URI = "neo4j+s://demo.neo4jlabs.com"
-    AUTH = (database, database)
+    database_uri = 'neo4j+s://demo.neo4jlabs.com'
 
-    query = """
+    query = '''
         MATCH (t:Tag {name: $tag})
         MATCH (q:Question)-[:TAGGED]->(t)
         WHERE toLower(q.title) CONTAINS $search 
@@ -34,29 +31,28 @@ def so_questions(event, context):
         RETURN q.title as title, q.body_markdown, q.link, answers
         ORDER BY q.view_count DESC
         LIMIT $limit
-    """
+    '''
 
-    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    with GraphDatabase.driver(database_uri, auth=(database, database)) as driver:
         with driver.session(database=database) as session:
             data = session.run(query, search=search, tag=tag, limit=limit)
             csv_string = data.to_df().to_csv()
 
     body = {
-        "inputs": json.dumps({"inputText":inputText, "params":params, "bodyParams":bodyParams}),
-        "message": "Top "+str(limit)+" questions for tag " + tag + " related to " + search + ":\n\n" 
-            + csv_string,
-        "input": event,
+        'inputs': json.dumps({'inputText': input_text, 'params': params, 'bodyParams': body}),
+        'message': 'Top ' + str(limit) + ' questions for tag ' + tag + ' related to ' + search + ":\n\n" + csv_string,
+        'input': event,
     }
     response = {
-    "messageVersion": "1.0",
-    "response": {
-        'actionGroup': event.get('actionGroup',''),
-        'apiPath': event.get('apiPath',''),
-        'httpMethod': event.get('httpMethod','GET'),
-        "httpStatusCode": 200,
-        "responseBody": {
-            "application/json": {
-                "body": json.dumps(body)
+        'messageVersion': '1.0',
+        'response': {
+            'actionGroup': event.get('actionGroup', ''),
+            'apiPath': event.get('apiPath', ''),
+            'httpMethod': event.get('httpMethod', 'GET'),
+            'httpStatusCode': 200,
+            'responseBody': {
+                'application/json': {
+                    'body': json.dumps(body)
                 }
             }
         }
@@ -64,4 +60,3 @@ def so_questions(event, context):
 
     logger.info('Response: {0}'.format(response))
     return response
-
